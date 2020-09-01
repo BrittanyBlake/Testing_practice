3245d7fe473b2ae83ab4565356f6b3d2f23df662
Brittany Blake <brittany.blake49@gmail.com>
2020-09-01 11:35:44 +0200

    add aditional tests for capitalize method

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -7,3 +7,24 @@ test('it should return the given string with the first character capitalized', (
 test("it should not return the given string with the last character capitalized", () => {
   expect(capitalize("hello")).not.toMatch("hellO");
 });
+
+describe("when only one character is given", () => {
+    test("it should capitalize the character", () => {
+        expect(capitalize("a")).toMatch("A");
+    });
+
+    test("it not should not return the character unchanged", () => {
+      expect(capitalize("a")).not.toMatch("a");
+    });
+})
+
+describe("when two words are given", () => {
+  test("it should capitalize the first character of the first word", () => {
+    expect(capitalize("good morning")).toMatch("Good morning");
+  });
+
+  test("it not should not capitalize the the first character of both words", () => {
+    expect(capitalize("good morning")).not.toMatch("Good Morning");
+  });
+});
+
